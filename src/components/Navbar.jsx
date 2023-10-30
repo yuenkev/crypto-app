0755878631ab071daf92832ec09d478dcea53469
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-10-29 21:51:57 -0400

    1st Commit - Setup + Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+//.jsx helps us differentiate 
+
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar />
+            <Typography.Title level={2} className='logo'>
+                <Link to="/">CryptoNet</Link>
+            </Typography.Title>
+            {/* <Button className='menu-control-container'>
+
+            </Button> */}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
