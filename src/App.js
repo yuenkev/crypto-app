7dfdd2d24f989a8f311bfbe74c811ef150e5ebbe
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-11-01 22:21:07 -0400

    2nd Commit - Navbar + Footer Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 // 'rafce' shortcut creates a boiler templates for react component
 
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
-import { Layout, Typography, Space } from 'antd';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-        <div className='navbar'>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
 
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reservered
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
-        <div className='main'>
-
-        </div>
-        <div className='footer'>
-            
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,31 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
+import icon from "../images/cryptocurrency.png"
+
 const Navbar = () => {
   return (
     <div className='nav-container'>
         <div className='logo-container'>
-            <Avatar />
+            <Avatar src={icon} size="large"/>
             <Typography.Title level={2} className='logo'>
                 <Link to="/">CryptoNet</Link>
             </Typography.Title>
-            {/* <Button className='menu-control-container'>
-
-            </Button> */}
         </div>
+        <Menu theme='dark'>
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/Cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
